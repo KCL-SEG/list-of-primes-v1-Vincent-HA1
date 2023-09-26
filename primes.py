d8b212330705a89f212ebdeb4c55cae3d46ea5f0
Vincent Ha <vincent.ha@kcl.ac.uk>
2023-09-26 15:12:52 +0100

    Complete number of primes function

--- primes.py
+++ primes.py
@@ -3,4 +3,7 @@
 
 def primes(number_of_primes):
     list = []
+    listOfPrimes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+    for i in range (0, number_of_primes):
+        list.append(listOfPrimes[i])
     return list
